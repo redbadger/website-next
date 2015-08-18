cbdb063f6ead0e61a35085b40199372186e514ee
Albert Still <albertjamesstill@gmail.com>
2015-08-18 12:07:46 +0100

    Cursor example with load more posts!

--- js/components/App.js
+++ js/components/App.js
@@ -2,7 +2,6 @@ import Post from './Post';
 
 class App extends React.Component {
   render() {
-    debugger
     return (
       <div>
         <h1>Post list</h1>
@@ -11,16 +10,25 @@ class App extends React.Component {
             <Post key={edge.node.id} post={edge.node} />
           )}
         </ul>
+        <button onClick={
+            () => { this.props.relay.setVariables({
+              count: this.props.relay.variables.count + 2
+            });
+          }
+        }>
+          Show more
+        </button>
       </div>
     );
   }
 }
 
 export default Relay.createContainer(App, {
+  initialVariables: {count: 5},
   fragments: {
     query: () => Relay.QL`
       fragment on Test {
-        posts(first: 4) {
+        posts(first: $count) {
           edges {
             node {
               id
