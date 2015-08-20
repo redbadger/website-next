8a4f72a9b2481ce539098c5d9a083c8c455c46ab
Alex Savin <karismafilms@gmail.com>
2015-08-20 16:21:37 +0100

    Assets pipeline kick off

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var del = require('del');
+var gulp = require('gulp');
+
+gulp.task('default', ['clean', 'copy-assets']);
+
+gulp.task('clean', function (cb) {
+  del.sync('public', {force: true});
+  cb();
+});
+
+gulp.task('copy-assets', function () {
+  gulp.src(['assets/css/**/*']).pipe(gulp.dest('public'));
+  gulp.src(['assets/favicons/*']).pipe(gulp.dest('public'));
+  gulp.src(['assets/images/**/*']).pipe(gulp.dest('public/images'));
+  gulp.src(['layouts/*']).pipe(gulp.dest('public'));
+});
+
