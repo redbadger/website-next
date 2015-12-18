616d1156a7fb909532fa7b4785d0c12e14a0b82e
RGBboy <l-_-l@rgbboy.com>
2015-12-18 09:19:18 +0000

    House keeping;

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('./babel-bootstrap');
 
 var app = require('./src/server').default;
 
-var server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(process.env.PORT || 8080, function () {
   console.log('Server listening on port', server.address().port);
 });
