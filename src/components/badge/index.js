247680b1e82b175f2cc78a17fb2a542a591a7860
RGBboy <l-_-l@rgbboy.com>
2015-12-22 14:27:34 +0000

    Add initial badge component;

---
+++ src/components/badge/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './style.css';
+import image from './badge.png';
+
+const Badge = function () {
+  return (
+    <span className={styles.badge}></span>
+  );
+};
+
+export default Badge;
