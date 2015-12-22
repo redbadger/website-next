d66fff7e9eccc241ba050ed1f812e08ae8bbe9d7
RGBboy <l-_-l@rgbboy.com>
2015-12-22 17:28:09 +0000

    Add initial grid component;

---
+++ src/components/grid/index.js
@@ -0,0 +1,38 @@
+/*eslint react/no-multi-comp:0*/
+import React from 'react';
+import classNames from 'classNames';
+import styles from './style.css';
+
+const gridClassNames = classNames(
+  styles.grid,
+  styles.withGutter,
+  styles.fit
+);
+
+export class Grid extends React.Component {
+  render () {
+    return (
+      <div className={gridClassNames}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Grid.propTypes = {
+  children: React.PropTypes.node
+};
+
+export class Cell extends React.Component {
+  render () {
+    return (
+      <div className={styles.cell}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Cell.propTypes = {
+  children: React.PropTypes.node
+};
