58686ca575bbdcc0b2690944d1cafb26e6c9e7e2
RGBboy <l-_-l@rgbboy.com>
2015-12-23 11:48:15 +0000

    Added initial button component;

---
+++ src/components/button/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './style.css';
+
+export default function Button (props) {
+  let { className, ...other } = props;
+  const buttonClass = classNames(className, styles.button);
+  if (props.href) {
+    return (
+      <a {...other} className={buttonClass}></a>
+    );
+  } else {
+    return (
+      <button {...other} className={buttonClass}></button>
+    );
+  }
+}
+
+Button.propTypes = {
+  className: React.PropTypes.string,
+  href: React.PropTypes.string
+};
