4c1cae0db2aabf26725dd75b8887b13ba285ffbe
Anna Doubkova <anna.doubkova@red-badger.com>
2016-01-05 13:23:48 +0000

    Set up unit testing framework

---
+++ src/components/test/test.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import sinon from 'sinon';
+
+const expect = chai.expect;
+
+describe('Test', function() {
+  it('Checks that 1 + 1 is 2', function() {
+    expect(1+1).to.equal(2);
+  });
+});
