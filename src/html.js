4514e8fc3f55731146451da31fa53aff83bdb127
Chris Shepherd <chris@chrisshepherd.me>
2016-01-05 13:36:32 +0000

    Added `indent` rule to .eslintrc

--- src/html.js
+++ src/html.js
@@ -1,18 +1,18 @@
 export default function (body) {
-return `
-<!doctype html>
-<html>
-  <head>
-  <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Red Badger</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/style.css" />
-  </head>
-  <body>
-    <div id="mount">${body}</div>
-    <script type="text/javascript" src="/client.js"></script>
-  </body>
-</html>
-`;
+  return `
+  <!doctype html>
+  <html>
+    <head>
+    <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Red Badger</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="/style.css" />
+    </head>
+    <body>
+      <div id="mount">${body}</div>
+      <script type="text/javascript" src="/client.js"></script>
+    </body>
+  </html>
+  `;
 }
