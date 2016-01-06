6cbed86e029f5767b217de05002fc8ea41f8addb
RGBboy <l-_-l@rgbboy.com>
2016-01-06 17:34:45 +0000

    Add craft background; Add paper tear background;

--- src/components/join-us/index.js
+++ src/components/join-us/index.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import classnames from 'classnames';
 import Content from '../content';
+import * as backgrounds from '../utils/background.css';
 
 class JoinUs extends Component {
   render () {
     return (
-      <Content>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <p>Paragraph. Lorem ipsum do sol.</p>
-      </Content>
+      <div className={classnames(backgrounds.craft, backgrounds.paperTear)}>
+        <Content>
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+          <p>Paragraph. Lorem ipsum do sol.</p>
+        </Content>
+      </div>
     );
   }
 }
