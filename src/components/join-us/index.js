702a0f41d1d97cba993fa74cd7b66ae4e39b477e
RGBboy <l-_-l@rgbboy.com>
2016-01-07 10:48:52 +0000

    Add how to apply section;

--- src/components/join-us/index.js
+++ src/components/join-us/index.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react';
-import classnames from 'classnames';
+import Container from '../container';
 import Content from '../content';
-import * as backgrounds from '../utils/background.css';
+import styles from './style.css';
+import * as textStyles from '../utils/text.css';
 
 class JoinUs extends Component {
   render () {
     return (
-      <div className={classnames(backgrounds.craft, backgrounds.paperTear)}>
-        <Content>
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <p>Paragraph. Lorem ipsum do sol.</p>
-        </Content>
+      <div className={styles.joinUs}>
+        <Container>
+          <Content>
+            <h2>How to Apply</h2>
+            <p className={textStyles.center}>
+              If you'd like more information, you want to apply, or you'd simply
+              like to say 'hello', then please get in touch with your CV,
+              Stackoverflow profile, Github, code, portfolio and anything else
+              you think we might be interested in,
+              at: <a href="mailto:jobs@red-badger.com">jobs@red-badger.com.</a>
+            </p>
+          </Content>
+        </Container>
       </div>
     );
   }
