27ad15bc725fd256f73e63d4c3ea9b5a62803cfc
RGBboy <l-_-l@rgbboy.com>
2016-01-07 10:55:25 +0000

    Wrap how to apply in section;

--- src/components/join-us/index.js
+++ src/components/join-us/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Container from '../container';
 import Content from '../content';
+import Section from '../section';
 import styles from './style.css';
 import * as textStyles from '../utils/text.css';
 
@@ -8,18 +9,20 @@ class JoinUs extends Component {
   render () {
     return (
       <div className={styles.joinUs}>
-        <Container>
-          <Content>
-            <h2>How to Apply</h2>
-            <p className={textStyles.center}>
-              If you'd like more information, you want to apply, or you'd simply
-              like to say 'hello', then please get in touch with your CV,
-              Stackoverflow profile, Github, code, portfolio and anything else
-              you think we might be interested in,
-              at: <a href="mailto:jobs@red-badger.com">jobs@red-badger.com.</a>
-            </p>
-          </Content>
-        </Container>
+        <Section>
+          <Container>
+            <Content>
+              <h2>How to Apply</h2>
+              <p className={textStyles.center}>
+                If you'd like more information, you want to apply, or you'd simply
+                like to say 'hello', then please get in touch with your CV,
+                Stackoverflow profile, Github, code, portfolio and anything else
+                you think we might be interested in,
+                at: <a href="mailto:jobs@red-badger.com">jobs@red-badger.com.</a>
+              </p>
+            </Content>
+          </Container>
+        </Section>
       </div>
     );
   }
