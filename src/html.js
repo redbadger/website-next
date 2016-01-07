e5b1d68b7baa4eef011b5138f628e14cb5c7b782
RGBboy <l-_-l@rgbboy.com>
2016-01-07 13:41:08 +0000

    House keeping - Indentation;

--- src/html.js
+++ src/html.js
@@ -3,7 +3,7 @@ export default function (body) {
   <!doctype html>
   <html>
     <head>
-    <meta charset="utf-8">
+      <meta charset="utf-8">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
       <title>Join Us | Red Badger</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
