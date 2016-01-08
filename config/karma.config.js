630307221a86df3e3274afb78e3a53a57042f285
Greg Stewart <gregs@tcias.co.uk>
2016-01-08 10:34:09 +0000

    Fix linting issues in karma conf

--- config/karma.config.js
+++ config/karma.config.js
@@ -1,39 +1,39 @@
 const webpack = require('webpack');
 const commonConfig = require('./common.webpack.config.js');
 
-module.exports = function(config) {
-    config.set({
-        browsers: ['Firefox'],
-        frameworks: ['mocha'],
-        files: [
-            '../src/**/*.spec.js'
-        ],
+module.exports = function (config) {
+  config.set({
+    browsers: ['Firefox'],
+    frameworks: ['mocha'],
+    files: [
+      '../src/**/*.spec.js'
+    ],
 
-        preprocessors: {
-            '../src/**/*.js': ['webpack']
-        },
+    preprocessors: {
+      '../src/**/*.js': ['webpack']
+    },
 
-        webpack: Object.assign({}, commonConfig, {
-          plugins: [
-            ...commonConfig.plugins,
-            new webpack.DefinePlugin({
-              'process.env': {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-              }
-            })
-          ]
-        }),
+    webpack: Object.assign({}, commonConfig, {
+      plugins: [
+        ...commonConfig.plugins,
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+          }
+        })
+      ]
+    }),
 
-        webpackMiddleware: {
-            noInfo: true
-        },
+    webpackMiddleware: {
+      noInfo: true
+    },
 
-        plugins: [
-            require("karma-webpack"),
-            require("karma-mocha"),
-            require("karma-firefox-launcher")
-        ],
-        singleRun: true
+    plugins: [
+      require("karma-webpack"),
+      require("karma-mocha"),
+      require("karma-firefox-launcher")
+    ],
+    singleRun: true
 
-    });
+  });
 };
