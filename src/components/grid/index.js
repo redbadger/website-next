496ee76193cb16b8f2f4b317c56da6a94612492f
RGBboy <l-_-l@rgbboy.com>
2016-01-18 17:33:09 +0000

    Add sizing to grid;

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -3,36 +3,44 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './style.css';
 
-const gridClassNames = classNames(
-  styles.grid,
-  styles.withGutter,
-  styles.fit
-);
-
-export class Grid extends React.Component {
-  render () {
-    return (
-      <div className={gridClassNames}>
-        {this.props.children}
-      </div>
-    );
+export function Grid (props) {
+  let fitClass;
+  let gridClassNames;
+  if (props.fit) {
+    fitClass = styles.fit;
   }
+  gridClassNames = classNames(
+   styles.grid,
+   styles.withGutter,
+   fitClass
+ );
+  return (
+    <div className={gridClassNames}>
+      {props.children}
+    </div>
+  );
 }
 
 Grid.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  fit: React.PropTypes.bool
 };
 
-export class Cell extends React.Component {
-  render () {
-    return (
-      <div className={styles.cell}>
-        {this.props.children}
-      </div>
-    );
+export function Cell (props) {
+  let sizeClass;
+  let cellClassNames;
+  if (props.size) {
+    sizeClass = styles['size' + props.size + 'of12'];
   }
+  cellClassNames = classNames(styles.cell, sizeClass);
+  return (
+    <div className={cellClassNames}>
+      {props.children}
+    </div>
+  );
 }
 
 Cell.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  size: React.PropTypes.number
 };
