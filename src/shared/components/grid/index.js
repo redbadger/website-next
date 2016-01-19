2154cced1937f238ef4ce9417f420c575b27c185
RGBboy <l-_-l@rgbboy.com>
2016-01-19 09:54:56 +0000

    Add Grid documentation;

--- src/shared/components/grid/index.js
+++ src/shared/components/grid/index.js
@@ -1,3 +1,21 @@
+/**
+ * Grid Component
+ *
+ * The grid component is used to create a grid layout. It should contain one
+ * or more Cell Components. The grid defaults to have gutters between Cells.
+ *
+ * Grid
+ * @props: {
+ *   fit: {Boolean}, make all cells fit into a single row
+ * }
+ *
+ * Cell
+ * @props: {
+ *   size: {Number}, number of columns to span out of 12
+ * }
+ *
+ */
+
 /*eslint react/no-multi-comp:0*/
 import React from 'react';
 import classNames from 'classnames';
