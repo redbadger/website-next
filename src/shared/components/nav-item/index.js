910452c45d31bdb778f99555e8fde2c7dc31eb44
RGBboy <l-_-l@rgbboy.com>
2016-01-21 13:42:26 +0000

    Revert using Link component for nav items;

--- src/shared/components/nav-item/index.js
+++ src/shared/components/nav-item/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './style.css';
 
-import { Link } from 'react-router';
-
 export default function NavItem ({active, href, title}) {
   let activeElement;
 
@@ -12,7 +10,7 @@ export default function NavItem ({active, href, title}) {
 
   return (
     <li className={styles.navItem}>
-      <Link className={styles.navItemLink} to={href}>{title}</Link>
+      <a className={styles.navItemLink} to={href}>{title}</a>
       {activeElement}
     </li>
   );
