ff627d6b877fef4e837124042525f1a8d15d52dd
RGBboy <l-_-l@rgbboy.com>
2016-01-22 14:35:41 +0000

    Abstract HtmlParser; Add styles to Job container;

--- src/shared/containers/job/index.js
+++ src/shared/containers/job/index.js
@@ -1,42 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import HtmlToReact from 'html-to-react';
+import HtmlParser from '../../components/html-parser';
 import { Grid, Cell } from '../../components/grid';
 import Container from '../../components/container';
 import Section from '../../components/section';
 import Note from '../../components/note';
+import styles from './style.css';
+import typography from '../../components/typography/style.css';
 import { Link } from 'react-router';
 import { filter, flow, head, property } from 'lodash/fp';
 import isEqual from 'lodash/isEqual'; // lodash fp isEqual is broken in 4.0.0
 
-const { parse } = new HtmlToReact.Parser(React);
-
 const howToApplyP = (
-  <p>
-    {`If you'd like more information, you want to apply, or you'd simply like to
-    say 'hello', then please get in touch with your CV, Stackoverflow profile,
-    Github, code, portfolio and anything else you think we might be interested
-    in, at: `}
-    <a href="mailto:jobs@red-badger.com">jobs@red-badger.com</a>
-    .
+  <p className={typography.p}>
+  If you'd like more information, you want to apply, or you'd simply like to
+  say 'hello', then please get in touch with your CV, Stackoverflow profile,
+  Github, code, portfolio and anything else you think we might be interested in,
+  at:{' '}<a className={typography.a} href="mailto:jobs@red-badger.com">jobs@red-badger.com</a>.
   </p>
 );
 
 export function Job ({ job }) {
-  const paragraphs = parse('<div>' + job.description + '</div>');
   return (
     <Section>
       <Container>
         <Grid>
           <Cell size={8}>
-            <h2>{job.title}</h2>
-            {paragraphs}
+            <h2 className={typography.h2}>{job.title}</h2>
+            <HtmlParser>{job.description}</HtmlParser>
             <hr />
-            <Link to="/about-us/join-us">See all vacancies</Link>
+            <Link className={typography.a} to="/about-us/join-us">See all vacancies</Link>
           </Cell>
           <Cell size={4}>
             <Note>
-              <h2>How to Apply</h2>
+              <h2 className={styles.noteTitle}>How to Apply</h2>
               {howToApplyP}
             </Note>
           </Cell>
