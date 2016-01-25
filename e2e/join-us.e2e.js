5ee1697ab40a6e7ae77613d28f998b5aedc05ddc
Anna Doubkova <anna.doubkova@red-badger.com>
2016-01-25 14:37:18 +0000

    Add E2E test for Job page

--- e2e/join-us.e2e.js
+++ e2e/join-us.e2e.js
@@ -3,12 +3,23 @@ import chai from 'chai';
 const browser = global.browser; // comes from wdio
 
 describe('Join Us page', function () {
-  it('loads', function (done) {
+  it('loads', function () {
     browser
       .url('/about-us/join-us')
       .getTitle()
       .then(function (title) {
         chai.expect(title).to.equal('Join Us | Red Badger');
+      });
+  });
+});
+
+describe('Job page', function () {
+  it('loads', function (done) {
+    browser
+      .url('/about-us/join-us/software-engineer')
+      .getText('h2')
+      .then(function (title) {
+        chai.expect(title).to.include('Software Engineer');
       })
       .end(done);
   });
