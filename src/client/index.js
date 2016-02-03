67ce5de0af0ab6ee40f0faefad1ac04898782309
Tiago Azevedo <tiago.azevedo@red-badger.com>
2016-02-03 15:23:09 +0000

    Uncomment clientside code, no longer dispatch action

--- src/client/index.js
+++ src/client/index.js
@@ -1,33 +1,29 @@
-// import getRoutes from '../shared/routes';
-// import reducers from '../shared/reducers';
-//
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import { Provider } from 'react-redux';
-// import { Router, browserHistory } from 'react-router';
-// import { syncHistory, routeReducer } from 'react-router-redux';
-//
-// const initialStateString = document.getElementById('initialState').textContent;
-// const initialState = JSON.parse(initialStateString);
-//
-// const reducer = combineReducers(Object.assign({}, reducers, {
-//   routing: routeReducer
-// }));
-//
-// const reduxRouterMiddleware = syncHistory(browserHistory);
-// const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore);
-// const store = createStoreWithMiddleware(reducer, initialState);
-//
-// const rootComponent = (
-//   <Provider store={store}>
-//     <Router history={browserHistory}>
-//       {getRoutes(store)}
-//     </Router>
-//   </Provider>
-// );
-//
-// const mountNode = document.getElementById('mount');
-//
-// ReactDOM.render(rootComponent, mountNode);
+import getRoutes from '../shared/routes';
+import reducers from '../shared/reducers';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistory } from 'react-router-redux';
+
+const initialStateString = document.getElementById('initialState').textContent;
+const initialState = JSON.parse(initialStateString);
+
+const reduxRouterMiddleware = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore);
+const store = createStoreWithMiddleware(reducers, initialState);
+
+const rootComponent = (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {getRoutes(store)}
+    </Router>
+  </Provider>
+);
+
+const mountNode = document.getElementById('mount');
+
+ReactDOM.render(rootComponent, mountNode);
--- src/server/middleware/router.js
+++ src/server/middleware/router.js
@@ -1,11 +1,25 @@
 import qs from 'query-string';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 
 import { createStore, applyMiddleware } from 'redux';
-import { match, createMemoryHistory } from 'react-router';
+import { match, createMemoryHistory, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
 import { syncHistory } from 'react-router-redux';
+import html from '../html';
 
 import reducers from '../../shared/reducers';
 import getRoutes from '../../shared/routes';
+
+const renderMarkup = (store, routerProps) => {
+  const htmlString = renderToString(
+    <Provider store={store}>
+      <RouterContext {...routerProps}/>
+    </Provider>
+  );
+
+  return html(htmlString, store.getState(), true);
+};
 
 export default((req, res) => {
   const history = createMemoryHistory();
@@ -15,16 +29,16 @@ export default((req, res) => {
   const query = qs.stringify(req.query);
   const url = req.path + (query.length ? '?' + query : '');
 
-  store.dispatch(match({ routes: getRoutes(store), location: url }, (error, redirectLocation, routerState) => {
+  match({ routes: getRoutes(store), location: url }, (error, redirectLocation, routerProps) => {
     if (error) {
       console.error('Routing error:', error);
       res.status(500).send(error.message);
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    } else if (!routerState) {
-      res.status(400).send('Not Found');
+    } else if (!routerProps) {
+      res.status(404).send('Not Found');
     } else {
-      res.status(200).send('Route found!');
+      res.status(200).send(renderMarkup(store, routerProps));
     }
-  }));
+  });
 });
