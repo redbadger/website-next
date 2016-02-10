fa37ef19893f7de8c05efcdf0b8e77e06a2f1649
Tiago Azevedo <tiago.azevedo@red-badger.com>
2016-02-10 11:23:17 +0000

    Stringify NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,7 @@ if (hot) {
 
 clientConfig.plugins.push(
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': process.env.NODE_ENV || "development"
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) || "development"
   })
 );
 
