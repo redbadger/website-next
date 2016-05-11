243c4371cd226ec5cda43e50f66165bb056f8fd1
Alex Savin <karismafilms@gmail.com>
2016-05-11 12:21:00 +0100

    Enabling Redux devtools

--- src/shared/create-store.js
+++ src/shared/create-store.js
@@ -2,9 +2,13 @@ import reducers from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import { syncHistory } from 'react-router-redux';
 import thunk from 'redux-thunk';
+import { env } from './config';
 
 export default (history, initialState) => {
   const reduxRouterMiddleware = syncHistory(history);
-  const store = applyMiddleware(thunk, reduxRouterMiddleware)(createStore)(reducers, initialState);
-  return store;
+  if (env === 'development') {
+    return applyMiddleware(thunk, reduxRouterMiddleware)(createStore)(reducers, initialState, typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f);
+  } else {
+    return applyMiddleware(thunk, reduxRouterMiddleware)(createStore)(reducers, initialState);
+  }
 };
