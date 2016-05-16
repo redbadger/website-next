e0ea6cef86005cf0f76a54344950a764139d0aeb
Alex Savin <karismafilms@gmail.com>
2016-05-16 15:32:01 +0100

    Events page styles with grid

--- src/shared/containers/events/index.js
+++ src/shared/containers/events/index.js
@@ -20,58 +20,68 @@ export class Events extends Component {
   render () {
     return (
       <div>
-        <div className={styles.apply}>
-          <Section>
-            <Container>
-              <h1 className={styles.h1}>Events</h1>
-              <ul className={styles.eventsList}>
-              {
-                this.props.events.map((event) => {
-                  return (
-                    <li key={`event_${event.id}`} className={styles.eventItem}>
-                      <HR color="grey" />
-                      <DateBubble
-                          date={event.doc.datetime.date}
-                          month={event.doc.datetime.month}
-                          year={event.doc.datetime.year}
-                      />
-                      <Grid fit={false} >
-                        <Cell size={8} key='event_description'>
-                          <Link href="#">
-                            <h2 className={styles.eventTitle}>
-                              {event.doc.attributes.title}
-                            </h2>
-                            <span className={classNames(
-                              {
-                                [styles.arrow]: true,
-                                [icons.sketchArrowRight]: true
-                              })}
-                            />
-                          </Link>
-                          <div>
-                            {event.doc.attributes.strapline}
-                          </div>
-                          <a href="#" className={styles.fullDetailsLink}>
-                              <span>For full details please visit</span>
-                              <span className={classNames({
-                                [icons.sketchExternalLink]: true,
-                                [styles.externalLinkIcon]: true
-                              })}
+        <Section>
+          <Container>
+            <h1 className={styles.h1}>Events</h1>
+            <ul className={styles.eventsList}>
+            {
+              this.props.events.map((event) => {
+                return (
+                  <li key={`event_${event.id}`} className={styles.eventItem}>
+                    <Grid fit={false} >
+                      <Cell size={1}>
+                        <DateBubble
+                            date={event.doc.datetime.date}
+                            month={event.doc.datetime.month}
+                            year={event.doc.datetime.year}
+                        />
+                      </Cell>
+                      <Cell size={11}>
+                        <HR color="grey" />
+                        <Grid fit={false}>
+                          <Cell size={8} key='event_description'>
+                            <Link href="#">
+                              <h2 className={styles.eventTitle}>
+                                {event.doc.attributes.title}
+                              </h2>
+                              <span className={classNames(
+                                {
+                                  [styles.arrow]: true,
+                                  [icons.sketchArrowRight]: true
+                                })}
                               />
-                          </a>
-                        </Cell>
-                        <Cell size={4} key='event_picture'>
-                          <img className={styles.eventPicture} src={ imageAssetsEndpoint + event.doc.attributes.featureImageFilename } />
-                        </Cell>
-                      </Grid>
-                    </li>
-                    );
-                })
-              }
-              </ul>
-            </Container>
-          </Section>
-        </div>
+                            </Link>
+                            <div>
+                              {event.doc.attributes.strapline}
+                            </div>
+                            <a href="#" className={styles.fullDetailsLink}>
+                                <span>For full details please visit</span>
+                                <span className={classNames({
+                                  [icons.sketchExternalLink]: true,
+                                  [styles.externalLinkIcon]: true
+                                })}
+                                />
+                            </a>
+                          </Cell>
+                          <Cell size={4} key='event_picture'>
+                            <div className={styles.imgBorder}>
+                              <div className={styles.imgWrapper}>
+                                <a href='#'>
+                                  <img className={styles.eventPicture} src={ imageAssetsEndpoint + event.doc.attributes.featureImageFilename } />
+                                </a>
+                              </div>
+                            </div>
+                          </Cell>
+                        </Grid>
+                      </Cell>
+                    </Grid>
+                  </li>
+                  );
+              })
+            }
+            </ul>
+          </Container>
+        </Section>
       </div>
     );
   }
