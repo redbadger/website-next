65c167e4c2917bbf6045de12a12fef2b47319c1d
Alex Savin <karismafilms@gmail.com>
2016-05-23 12:28:38 +0100

    Splitting events list into past and future events
    Now featuring 2 sections with past and future events on the
    /about-us/events page. View layer is taking care of the actual splitting
    of events based on the client's date and date of the event.

--- src/server/api/routes.js
+++ src/server/api/routes.js
@@ -1,11 +1,5 @@
 import { couchDbLocal, couchDbRemote } from '../../shared/config';
 
-// Using old school require because of https://github.com/moment/moment/issues/2608
-const moment = require('moment');
-
-const today = moment().toISOString();
-const fiveYearsFromNow = moment().add(5, 'years').toISOString();
-
 export default class Routes {
   constructor (workable) {
     this.workable = workable;
@@ -20,7 +14,7 @@ export default class Routes {
   };
 
   getEvents = (req, res) => {
-    fetch((process.env.NODE_ENV === 'production' ? couchDbRemote : couchDbLocal) + '/events/_design/date/_view/by_date?include_docs=true&startkey="' + today + '"&endkey="' + fiveYearsFromNow + '"')
+    fetch((process.env.NODE_ENV === 'production' ? couchDbRemote : couchDbLocal) + '/events/_all_docs?include_docs=true')
       .then((response) => {
         return response.json();
       })
