802bab3395bc2975c2a074fb7ada918f8f31d292
Toby Merlet <tobymerlet@Tobys-MacBook-Pro-2.local>
2016-05-27 11:56:18 +0100

    minimum workable styles

--- src/shared/containers/add-event/index.js
+++ src/shared/containers/add-event/index.js
@@ -20,22 +20,22 @@ export class AddEvents extends Component {
               : []
             }
             <form action="/api/new-event" method="post">
-              <h4 className={styles.h4}>Title:</h4>
-              <input name="title" type="text"/><br/><br/>
-              <h4 className={styles.h4}>Strapline:</h4>
-              <input name="strapline" type="text"></input><br/><br/>
-              <h4 className={styles.h4}>FeatureImage Filename (or cloudinary link):</h4>
-              <input name="featureImageFilename" type="text"></input><br/><br/>
-              <h4 className={styles.h4}>Body:</h4>
-              <textarea name="body" type="text"></textarea><br/><br/>
-              <h4 className={styles.h4}>Date and Time:</h4>
+              <h4>Title:</h4>
+              <input className={styles.eventInputBox} name="title" type="text"/><br/><br/>
+              <h4>Strapline:</h4>
+              <input className={styles.eventInputBox} name="strapline" type="text"></input><br/><br/>
+              <h4>Feature-Image Filename (or cloudinary link):</h4>
+              <input className={styles.eventInputBox} name="featureImageFilename" type="text"></input><br/><br/>
+              <h4>Body:</h4>
+              <textarea className={styles.eventTextArea} name="body" type="text"></textarea><br/><br/>
+              <h4>Date and Time:</h4>
               <input name="eventDate"  type="date"></input><input name="eventTime" type="time"></input><br/><br/>
-              <h4 className={styles.h4}>External link:</h4>
-              <span>Title: </span><input name="externalTitle" type="text"></input><br/><br/>
-              <span>URL: </span><input name="externalUrl" type="text"></input><br/><br/>
-              <h4 className={styles.h4}>Internal link:</h4>
-              <span>Title: </span><input name="internalTitle" type="text"></input><br/><br/>
-              <span>URL: </span><input name="internalUrl" type="text"></input><br/><br/>
+              <h4>External link:</h4>
+              <span>Title: </span><input className={styles.eventInputBox} name="externalTitle" type="text"></input><br/><br/>
+              <span>URL: </span><input className={styles.eventInputBox} name="externalUrl" type="text"></input><br/><br/>
+              <h4>Internal link:</h4>
+              <span>Title: </span><input className={styles.eventInputBox} name="internalTitle" type="text"></input><br/><br/>
+              <span>URL: </span><input className={styles.eventInputBox} name="internalUrl" type="text"></input><br/><br/>
               <input type="submit" value="Submit" />
             </form>
           </Container>
