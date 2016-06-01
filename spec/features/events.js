40567886922ba0156276961dfbb2e53ab44e3d24
Seb Flippence <seb.flippence@red-badger.com>
2016-06-01 15:51:37 +0100

    Circle CI try using: global.browser

--- spec/features/events.js
+++ spec/features/events.js
@@ -1,30 +1,25 @@
-import webdriverio from 'webdriverio';
 import chai from 'chai';
-import { config } from '../wdio.config';
+
+const browser = global.browser; // comes from wdio
 
 describe('Checking for Events', () => {
-  let client;
-
-  before(() => {
-    client = webdriverio.remote(config);
-    return client.init();
+  it('will be able to view upcoming events', done => {
+    browser
+      .url('/about-us/events')
+      .getTitle()
+      .then(function (title) {
+        chai.expect(title).to.equal('Events | Red Badger');
+      })
+      .end(done);
   });
 
-  after(() => client.end());
-
-  it('will be able to view upcoming events', () => client
-    .url('http://localhost:8000/about-us/events')
-    .getTitle()
-    .then(function (title) {
-      chai.expect(title).to.equal('Events | Red Badger');
-    })
-  );
-
-  it('redirects to Google login page, when you try to add an event and are not logged in', () => client
-    .url('/about-us/events/add')
-    .getTitle()
-    .then(function (title) {
-      chai.expect(title).to.equal('Sign in - Google Accounts');
-    })
-  );
+  it('redirects to Google login page, when you try to add an event and are not logged in', done => {
+    browser
+      .url('/about-us/events/add')
+      .getTitle()
+      .then(function (title) {
+        chai.expect(title).to.equal('Sign in - Google Accounts');
+      })
+      .end(done);
+  });
 });
