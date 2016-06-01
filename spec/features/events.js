baf1ba11f9e627a7fbf59fffb3505fb9a83fabd5
Seb Flippence <seb.flippence@red-badger.com>
2016-06-01 16:25:31 +0100

    Revert "Circle CI try using: global.browser"
    This reverts commit 40567886922ba0156276961dfbb2e53ab44e3d24.

--- spec/features/events.js
+++ spec/features/events.js
@@ -1,25 +1,30 @@
+import webdriverio from 'webdriverio';
 import chai from 'chai';
-
-const browser = global.browser; // comes from wdio
+import { config } from '../wdio.config';
 
 describe('Checking for Events', () => {
-  it('will be able to view upcoming events', done => {
-    browser
-      .url('/about-us/events')
-      .getTitle()
-      .then(function (title) {
-        chai.expect(title).to.equal('Events | Red Badger');
-      })
-      .end(done);
+  let client;
+
+  before(() => {
+    client = webdriverio.remote(config);
+    return client.init();
   });
 
-  it('redirects to Google login page, when you try to add an event and are not logged in', done => {
-    browser
-      .url('/about-us/events/add')
-      .getTitle()
-      .then(function (title) {
-        chai.expect(title).to.equal('Sign in - Google Accounts');
-      })
-      .end(done);
-  });
+  after(() => client.end());
+
+  it('will be able to view upcoming events', () => client
+    .url('http://localhost:8000/about-us/events')
+    .getTitle()
+    .then(function (title) {
+      chai.expect(title).to.equal('Events | Red Badger');
+    })
+  );
+
+  it('redirects to Google login page, when you try to add an event and are not logged in', () => client
+    .url('/about-us/events/add')
+    .getTitle()
+    .then(function (title) {
+      chai.expect(title).to.equal('Sign in - Google Accounts');
+    })
+  );
 });
