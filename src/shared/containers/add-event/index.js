8964ecd41592244d41ffda3c29e090f23c4c3973
Alex Savin <karismafilms@gmail.com>
2016-06-03 10:38:29 +0100

    UX fixes for add event form

--- src/shared/containers/add-event/index.js
+++ src/shared/containers/add-event/index.js
@@ -15,6 +15,9 @@ export class AddEvents extends Component {
         <Section>
           <Container>
             <h1 className={styles.h1}>Add Event</h1>
+            <p>
+              All fields are mandatory
+            </p>
             {error ?
               <h1 className={styles.warning}>Something went wrong</h1>
               : []
@@ -24,18 +27,18 @@ export class AddEvents extends Component {
               <input className={styles.eventInputBox} name="title" type="text"/><br/><br/>
               <h4>Strapline:</h4>
               <input className={styles.eventInputBox} name="strapline" type="text"></input><br/><br/>
-              <h4>Feature-Image Filename (or cloudinary link):</h4>
+              <h4>Cloudinary image filename:</h4>
               <input className={styles.eventInputBox} name="featureImageFilename" type="text"></input><br/><br/>
-              <h4>Body:</h4>
+              <h4>Body with markdown:</h4>
               <textarea className={styles.eventTextArea} name="body" type="text"></textarea><br/><br/>
               <h4>Date and Time:</h4>
               <input name="eventDate"  type="date"></input><input name="eventTime" type="time"></input><br/><br/>
-              <h4>External link:</h4>
+              <h4>External link (optional):</h4>
               <span>Title: </span><input className={styles.eventInputBox} name="externalTitle" type="text"></input><br/><br/>
-              <span>URL: </span><input className={styles.eventInputBox} name="externalUrl" type="text"></input><br/><br/>
-              <h4>Internal link:</h4>
+              <span>Full URL</span><input className={styles.eventInputBox} name="externalUrl" type="text"></input><br/><br/>
+              <h4>Internal link (optional):</h4>
               <span>Title: </span><input className={styles.eventInputBox} name="internalTitle" type="text"></input><br/><br/>
-              <span>URL: </span><input className={styles.eventInputBox} name="internalUrl" type="text"></input><br/><br/>
+              <span>Full URL</span><input className={styles.eventInputBox} name="internalUrl" type="text"></input><br/><br/>
               <input type="submit" value="Submit" />
             </form>
           </Container>
