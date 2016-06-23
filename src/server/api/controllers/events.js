b55c028eb18883d155462969e298c432a6a7f4a3
Chris Goodchild <seegoodchild@gmail.com>
2016-06-23 14:57:36 +0100

    added a controller action to get a single event by id from Badger Brain

--- src/server/api/controllers/events.js
+++ src/server/api/controllers/events.js
@@ -1,48 +1,76 @@
 import { badgerBrain } from '../../../shared/config';
 
+const badgerBrainEndpoint =
+  process.env.NODE_ENV === 'production'
+  ? badgerBrain.remote
+  : badgerBrain.local;
+
+const getRequestOptions = (body) => ({
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/graphql',
+  },
+  body,
+});
+
+const allEventFields = `
+  id
+  slug
+  title
+  strapline
+  internalLinks {
+    title
+    url
+  }
+  externalLinks {
+    title
+    url
+  }
+  datetime {
+    iso
+    date
+    month
+    monthSym
+    year
+  }
+  body {
+    type
+    text
+  }
+  featureImageFilename
+`;
+
 export default class EventsController {
-  getEvents = (req, res) => {
-    const fetchInit = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/graphql',
-      },
-      body: `
-        query {
-          allEvents {
-            id
-            slug
-            title
-            strapline
-            internalLinks {
-              title
-              url
-            }
-            externalLinks {
-              title
-              url
-            }
-            datetime {
-              iso
-              date
-              month
-              monthSym
-              year
-            }
-            body {
-              type
-              text
-            }
-            featureImageFilename
-          }
+  getEvent = (req, res) => {
+    const body = `
+      query {
+        event(id: "${req.query.id}") {
+          ${allEventFields}
         }
-      `,
-    };
+      }
+    `;
 
-    const endpoint = process.env.NODE_ENV === 'production'
-      ? badgerBrain.remote : badgerBrain.local;
+    fetch(badgerBrainEndpoint, getRequestOptions(body))
+      .then((response) => (
+        response.json().then((json) => {
+          res.send(json.data.event);
+        })
+      ))
+      .catch((err) => {
+        res.status(err.status).send(err.message);
+      });
+  };
 
-    fetch(endpoint, fetchInit)
+  getEvents = (req, res) => {
+    const body = `
+      query {
+        allEvents {
+          ${allEventFields}
+        }
+      }
+    `;
+
+    fetch(badgerBrainEndpoint, getRequestOptions(body))
       .then((response) => response.json())
       .then((events) => {
         res.send({ list: events.data.allEvents.sort((a, b) =>
