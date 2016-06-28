59de03bdd3e4f4321629f405cea8e490f3db40f5
Alexander Savin <asavin@users.noreply.github.com>
2016-06-28 14:44:59 +0100

    Revert "Add Prismic preview token"

--- src/server/preview/linkResolvers.js
+++ src/server/preview/linkResolvers.js
@@ -4,11 +4,11 @@
   in the document schema and constructs the path using the provided document.
 */
 const asPreview = (resolver) => (
-  (token, doc) => {
+  (doc) => {
     let path = '/';
 
     try {
-      path += `${resolver(doc)}?id=${doc.id}&token=${token}`;
+      path += `${resolver(doc)}?preview=${doc.id}`;
     } catch (error) {
       console.warn(`Could not create path: ${error.message}`);
     }
@@ -16,20 +16,15 @@ const asPreview = (resolver) => (
   }
 );
 
-export const resolvers = {
-  event: asPreview((doc) => {
-    const datePath = doc.data['event.timestamp'].value
-      .match(/^(\d{4})-(\d{2})-(\d{2})/)
-      .slice(1)
-      .join('/');
+const event = asPreview((doc) => {
+  const datePath = doc.data['event.timestamp'].value
+    .match(/^(\d{4})-(\d{2})-(\d{2})/)
+    .slice(1)
+    .join('/');
 
-    return `about-us/events/${datePath}/${doc.uid}`;
-  }),
+  return `about-us/events/${datePath}/${doc.uid}`;
+});
+
+export default {
+  event,
 };
-
-export default function linkResolver(token, doc) {
-  if (typeof resolvers[doc.type] === 'function') {
-    return resolvers[doc.type](token, doc);
-  }
-  return '/';
-}
