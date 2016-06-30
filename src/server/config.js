d1452f95e6a3ef3ed884b6e653f7703c5812556a
Chris Goodchild <seegoodchild@gmail.com>
2016-06-30 16:40:36 +0100

    changed NODE_ENV to APP_ENV so that the correct Prismic repository is used

--- src/server/config.js
+++ src/server/config.js
@@ -9,7 +9,7 @@ export const hot = process.env.HOT !== undefined;
 export const port = process.env.PORT || 8000;
 
 export const prismicApiEndpoint = (
-  process.env.NODE_ENV === 'production'
+  process.env.APP_ENV === 'production'
     ? 'https://rb-website.prismic.io/api'
     : 'https://rb-website-stage.prismic.io/api'
 );
