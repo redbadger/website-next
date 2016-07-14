845a49b2fc40355384dd1486c077792196f3bf20
Alex Savin <karismafilms@gmail.com>
2016-07-14 17:07:46 +0100

    Updating split events logic to accommodate multiday events. With tests.

--- src/shared/util/split-events.js
+++ src/shared/util/split-events.js
@@ -1,20 +1,43 @@
 import dateFns from 'date-fns';
 
-export function splitEvents(events, timeline, { reverse = false } = {}) {
+export function splitEvents({
+  events,              // array of events
+  timeline,            // one of 'past', 'today', 'future'
+  reverse = false,     // optionally reverse final list of events
+  todayDateTime = null,  // iso string representing today date
+}) {
+  const today = todayDateTime || new Date();
+
   let relevantEvents = events.filter(event => {
-    const d = dateFns.parse(event.startDateTime.iso);
+    const startDateTime = dateFns.parse(event.startDateTime.iso);
+    const endDateTime = dateFns.parse(event.endDateTime.iso);
+
+    // In a rare case of user error we omit this event from the output list
+    if (!dateFns.isSameDay(startDateTime, endDateTime) &&
+      !dateFns.isBefore(startDateTime, endDateTime)) {
+      return false;
+    }
+
     switch (timeline) {
       case 'today':
-        return dateFns.isToday(d);
+        if (dateFns.isSameDay(startDateTime, endDateTime)) {
+          return dateFns.isSameDay(startDateTime, today);
+        } else { // eslint-disable-line no-else-return
+          return dateFns.isWithinRange(today, startDateTime, endDateTime);
+        }
       case 'past':
-        return (dateFns.isPast(d) && !dateFns.isToday(d));
+        return (!dateFns.isWithinRange(today, startDateTime, endDateTime) && dateFns.isBefore(endDateTime, today) && !dateFns.isSameDay(endDateTime, today));
+      case 'future':
+        return (!dateFns.isSameDay(startDateTime, today) &&
+          dateFns.isAfter(startDateTime, today));
       default:
-        return (dateFns.isFuture(d) && !dateFns.isToday(d));
+        return false;
     }
   }, this);
 
   if (relevantEvents.length > 1 && reverse === true) {
     relevantEvents = relevantEvents.reverse();
   }
+
   return relevantEvents;
 }
--- src/shared/util/split-events.spec.js
+++ src/shared/util/split-events.spec.js
@@ -2,13 +2,17 @@ import { splitEvents } from './split-events';
 import { expect } from 'chai';
 import * as MockDate from 'mockdate';
 
-let testevents = [];
+let testEventsSingleDay = [];
+let testEventsMultiDay = [];
+let testEventsMultiDayInProgress = [];
+let testErrorMultiDay = [];
+let currentDate;
 
 describe('SplitEvents', () => {
   beforeEach(() => {
     MockDate.set('Thu Jun 23 2016 14:10:56 GMT+0100 (BST)');
 
-    const currentDate = new Date();
+    currentDate = new Date();
 
     const laterToday = new Date();
     laterToday.setHours(currentDate.getHours() + 1);
@@ -22,7 +26,79 @@ describe('SplitEvents', () => {
     const previousDate = new Date();
     previousDate.setDate(previousDate.getDate() - 5);
 
-    testevents = [
+    const threeDaysAgo = new Date();
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    testEventsMultiDay = [
+      {
+        id: 'past-multi-day-event',
+        startDateTime: {
+          iso: previousDate,
+        },
+        endDateTime: {
+          iso: threeDaysAgo,
+        },
+      },
+      {
+        id: 'today-multi-day-event',
+        startDateTime: {
+          iso: currentDate,
+        },
+        endDateTime: {
+          iso: futureDate,
+        },
+      },
+      {
+        id: 'future-multi-day-event',
+        startDateTime: {
+          iso: tomorrow,
+        },
+        endDateTime: {
+          iso: futureDate,
+        },
+      },
+    ];
+
+    testErrorMultiDay = [
+      {
+        id: 'today-multi-day-event',
+        startDateTime: {
+          iso: tomorrow,
+        },
+        endDateTime: {
+          iso: yesterday,
+        },
+      },
+    ];
+
+    testEventsMultiDayInProgress = [
+      {
+        id: 'past-multi-day-event',
+        startDateTime: {
+          iso: previousDate,
+        },
+        endDateTime: {
+          iso: threeDaysAgo,
+        },
+      },
+      {
+        id: 'yesterday-tomorrow-multi-day-event',
+        startDateTime: {
+          iso: yesterday,
+        },
+        endDateTime: {
+          iso: futureDate,
+        },
+      },
+    ];
+
+    testEventsSingleDay = [
       {
         id: 'past-event-1',
         startDateTime: {
@@ -102,37 +178,112 @@ describe('SplitEvents', () => {
     MockDate.reset();
   });
 
-  it('returns future events', () => {
-    const timeline = 'future';
-    const returnedEvents = splitEvents(testevents, timeline);
-    expect(returnedEvents.length).to.equal(2);
-    expect(returnedEvents[0].id).to.equal('future-event-1');
-    expect(returnedEvents[1].id).to.equal('future-event-2');
+  describe('multi day events', () => {
+    it('omits events with end date earlier then start date from final list', () => {
+      const timeline = 'today';
+      const returnedEvents = splitEvents({
+        events: testErrorMultiDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(0);
+    });
+
+    it('returns todays events correctly when event start date is today and end date is tomorrow', () => {
+      const timeline = 'today';
+      const returnedEvents = splitEvents({
+        events: testEventsMultiDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(1);
+      expect(returnedEvents[0].id).to.equal('today-multi-day-event');
+    });
+
+    it('returns todays events correctly when event start date was yesterday and end date is tomorrow', () => {
+      const timeline = 'today';
+      const returnedEvents = splitEvents({
+        events: testEventsMultiDayInProgress,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(1);
+      expect(returnedEvents[0].id).to.equal('yesterday-tomorrow-multi-day-event');
+    });
+
+    it('returns future events correctly for upcoming multiday events', () => {
+      const timeline = 'future';
+      const returnedEvents = splitEvents({
+        events: testEventsMultiDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(1);
+      expect(returnedEvents[0].id).to.equal('future-multi-day-event');
+    });
+
+    it('returns past events correctly for multiday events', () => {
+      const timeline = 'past';
+      const returnedEvents = splitEvents({
+        events: testEventsMultiDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(1);
+      expect(returnedEvents[0].id).to.equal('past-multi-day-event');
+    });
   });
 
-  it('returns past events', () => {
-    const timeline = 'past';
-    const returnedEvents = splitEvents(testevents, timeline);
-    expect(returnedEvents.length).to.equal(2);
-    expect(returnedEvents[0].id).to.equal('past-event-1');
-    expect(returnedEvents[1].id).to.equal('past-event-2');
-  });
+  describe('single day events', () => {
+    it('returns future events', () => {
+      const timeline = 'future';
+      const returnedEvents = splitEvents({
+        events: testEventsSingleDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(2);
+      expect(returnedEvents[0].id).to.equal('future-event-1');
+      expect(returnedEvents[1].id).to.equal('future-event-2');
+    });
 
-  it('returns todays events', () => {
-    const timeline = 'today';
-    const returnedEvents = splitEvents(testevents, timeline);
-    expect(returnedEvents.length).to.equal(4);
-    expect(returnedEvents[0].id).to.equal('today-event-1');
-    expect(returnedEvents[1].id).to.equal('today-event-2');
-    expect(returnedEvents[2].id).to.equal('later-today-event');
-    expect(returnedEvents[3].id).to.equal('earlier-today-event');
-  });
+    it('returns past events', () => {
+      const timeline = 'past';
+      const returnedEvents = splitEvents({
+        events: testEventsSingleDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(2);
+      expect(returnedEvents[0].id).to.equal('past-event-1');
+      expect(returnedEvents[1].id).to.equal('past-event-2');
+    });
 
-  it('returns events in reverse order when specified', () => {
-    const timeline = 'future';
-    const returnedEvents = splitEvents(testevents, timeline, { reverse: true });
-    expect(returnedEvents.length).to.equal(2);
-    expect(returnedEvents[0].id).to.equal('future-event-2');
-    expect(returnedEvents[1].id).to.equal('future-event-1');
+    it('returns todays events', () => {
+      const timeline = 'today';
+      const returnedEvents = splitEvents({
+        events: testEventsSingleDay,
+        timeline,
+        todayDateTime: currentDate,
+      });
+      expect(returnedEvents.length).to.equal(4);
+      expect(returnedEvents[0].id).to.equal('today-event-1');
+      expect(returnedEvents[1].id).to.equal('today-event-2');
+      expect(returnedEvents[2].id).to.equal('later-today-event');
+      expect(returnedEvents[3].id).to.equal('earlier-today-event');
+    });
+
+    it('returns events in reverse order when specified', () => {
+      const timeline = 'future';
+      const returnedEvents = splitEvents({
+        events: testEventsSingleDay,
+        timeline,
+        todayDateTime: currentDate,
+        reverse: true,
+      });
+      expect(returnedEvents.length).to.equal(2);
+      expect(returnedEvents[0].id).to.equal('future-event-2');
+      expect(returnedEvents[1].id).to.equal('future-event-1');
+    });
   });
 });
