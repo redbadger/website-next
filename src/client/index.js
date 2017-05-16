972ae657e600c7f36656cc134d4f102e17d6fd45
Dominik Stodolny <dom@dmk.st>
2017-05-16 15:03:01 +0100

    improve accessibility bar

--- src/client/index.js
+++ src/client/index.js
@@ -19,6 +19,29 @@ const store = createStore(browserHistory, initialState);
 
 const mountNode = document.getElementById('mount');
 
+const handleClick = () => {
+  const colourOnButtonText = 'Make my day accessible';
+  const colourOnDescription = 'Lorem ipsum dolor';
+  const colourOffButtonText = 'Gimme back my colour';
+  const colourOffDescription = 'Some text about what this is, Global Accessibility Day etc';
+
+  const button = document.getElementById('accessibilityDayButton');
+  const description = document.getElementById('accessibilityDayDescription');
+
+  document.documentElement.classList.toggle('accessibilityDay');
+
+  if (button.innerHTML === colourOffButtonText) {
+    button.innerHTML = colourOnButtonText;
+  } else {
+    button.innerHTML = colourOffButtonText;
+  }
+  if (description.innerHTML === colourOffDescription) {
+    description.innerHTML = colourOnDescription;
+  } else {
+    description.innerHTML = colourOffDescription;
+  }
+};
+
 match({
   routes: getRoutes(store),
   location: window.location,
@@ -37,14 +60,16 @@ match({
   const Application = (
     <div>
       <div className={styles.accessibilityBar}>
-        <button onClick={() => document.documentElement.classList.toggle('accessibilityDay')}>Gimme back my colour</button>
-        <p>Some text about what this is, Global Accessibility Day etc</p>
+        <button id="accessibilityDayButton" onClick={handleClick}>Gimme back my colour</button>
+        <p id="accessibilityDayDescription">Some text about what this is, Global Accessibility Day etc</p>
       </div>
-      <Provider store={store}>
-        <Router {...renderProps}
-          history={browserHistory}
-          onUpdate={routerUpdate} />
-      </Provider>
+      <div className={styles.app}>
+        <Provider store={store}>
+          <Router {...renderProps}
+            history={browserHistory}
+            onUpdate={routerUpdate} />
+        </Provider>
+      </div>
     </div>
   );
 
